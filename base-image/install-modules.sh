48f36061d13e7b81100734ee1fa6bc28aa93f0c1
Corey Gillen <corey.j.gillen@gmail.com>
2019-04-23 11:31:32 -0700

    Feature/google analytics (#13)
    * Add ability to rename module folders
    
    * Add Google Analytics module to mellon

--- base-image/install-modules.sh
+++ base-image/install-modules.sh
@@ -2,10 +2,14 @@
 
 # Install Omeka modules
 if [[ -n "${MODULE_FILE}" ]] && [[ -f "${MODULE_FILE}" ]]; then
-  while read m; do
-    echo "$0: Installing module: $m"
-    curl -sSL "$m" -o "module.zip"
+  while IFS=';' read -ra m; do
+    echo "$0: Installing module: ${m[0]}"
+    curl -sSL "${m[0]}" -o "module.zip"
     unzip -qn module.zip -d ${OMEKA_PATH}/modules/
     rm module.zip
+    if [[ ! -z ${m[1]} ]] && [[ ! -z ${m[2]} ]]; then
+      echo "$0: Renaming module directory '${m[1]}': to '${m[2]}'"
+      mv ${OMEKA_PATH}/modules/${m[1]} ${OMEKA_PATH}/modules/${m[2]}
+    fi
   done < ${MODULE_FILE}
 fi
