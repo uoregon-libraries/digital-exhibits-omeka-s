af284975ee82e4eb805076bead91472f67f630ae
Corey Gillen <gillenc@uoregon.edu>
2019-05-16 08:02:29 -0700

    Update and pin UO Libraries theme

--- base-image/install-themes.sh
+++ base-image/install-themes.sh
@@ -2,10 +2,14 @@
 
 # Install Omeka themes
 if [[ -n "${THEME_FILE}" ]] && [[ -f "${THEME_FILE}" ]]; then
-  while read t; do
-    echo "$0: Installing theme: $t"
-    curl -sSL "$t" -o "theme.zip"
+  while IFS=';' read -ra m; do
+    echo "$0: Installing theme: ${m[0]}"
+    curl -sSL "${m[0]}" -o "theme.zip"
     unzip -qn theme.zip -d ${OMEKA_PATH}/themes/
     rm theme.zip
+    if [[ ! -z ${m[1]} ]] && [[ ! -z ${m[2]} ]]; then
+      echo "$0: Renaming theme directory '${m[1]}': to '${m[2]}'"
+      mv ${OMEKA_PATH}/themes/${m[1]} ${OMEKA_PATH}/themes/${m[2]}
+    fi
   done < ${THEME_FILE}
 fi
